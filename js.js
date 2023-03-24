ce3bfe6ee374d23c42174efd55a949ccdcc67e50
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-03-25 02:47:39 +0530

    registration form step-01

--- js.js
+++ js.js
@@ -1,58 +1,123 @@
-// External
+// // External
 
-// varible
+// // varible
 
-var firstName = 'Chinthaka';
-var lastName = 'Senarathna';
-var age = 22;
-var isOk = true;
-var job,height;
+// var firstName = 'Chinthaka';
+// var lastName = 'Senarathna';
+// var age = 22;
+// var isOk = true;
+// var job,height;
 
-// print varible
-console.log(firstName);
+// // print varible
+// console.log(firstName);
 
-// string concatination
-console.log('My name is '+firstNam+' '+lastName);
+// // string concatination
+// console.log('My name is '+firstNam+' '+lastName);
 
-//  type casting / coercion
-console.log('My age is '+age);
+// //  type casting / coercion
+// console.log('My age is '+age);
 
-// variable type
-console.log(typeof(firstName));
-console.log(typeof(age));
+// // variable type
+// console.log(typeof(firstName));
+// console.log(typeof(age));
 
-// define
-job = 'Software Engineer';
+// // define
+// job = 'Software Engineer';
 
-// variable mutation
-firstName = 'Parami';
+// // variable mutation
+// firstName = 'Parami';
 
-// display alert
-alert('My name is '+firstNam+' '+lastName+','+' My age is '+age+' & '+'My job is '+job);
+// // display alert
+// alert('My name is '+firstNam+' '+lastName+','+' My age is '+age+' & '+'My job is '+job);
 
-// get user input
-height = prompt('What is your height ');
+// // get user input
+// height = prompt('What is your height ');
 
-// display
-console.log('My height is '+height+' feets');
+// // display
+// console.log('My height is '+height+' feets');
 
 
-// Arithmetic Operators
+// // Arithmetic Operators
 
-// = , + , - , * , / , % , **
-// variable++(post increment) , ++variable(pre increment)
-// variable--(post decrement) , --variable(pre decrement)
+// // = , + , - , * , / , % , **
+// // variable++(post increment) , ++variable(pre increment)
+// // variable--(post decrement) , --variable(pre decrement)
 
-// operator precedence
-var cal = (20+30)/10+4.5-2*4**2;
+// // operator precedence
+// var cal = (20+30)/10+4.5-2*4**2;
 
-// multiple operator
+// // multiple operator
 
-var ans1,ans2;
-ans1 = ans2 = (((20+30-49)*10)/2)**2;
-console.log(ans1 + ' | ' + ans2);
+// var ans1,ans2;
+// ans1 = ans2 = (((20+30-49)*10)/2)**2;
+// console.log(ans1 + ' | ' + ans2);
 
-// Assignment óperators
-// = , += , -= , *= , /= , %= , **=
+// // Assignment óperators
+// // = , += , -= , *= , /= , %= , **=
+
+
+
+
+
+
+
+// ------How to break line,
+// var age = 22;
+// fname = 'Chinthaka';
+// lname = 'Senarathna';
+// university = 'UOM';
+
+// document.write(age);
+// document.write(fname);
+// document.write(lname);
+// document.write(university);
+
+// document.write("<br>"+age+"<br>");
+// document.write(fname+"<br>");
+// document.write(lname+"<br>");
+// document.write(university+"<br>");
+//
+
+
+// -------------------------------------JavaScript Form Validation                       
+// function allnumeric(inputtxt){
+//     var number = /^[0-9]+$/;
+//     if(inputtxt.value.match(number)){
+//         alert("Your Registration number has accepted....");
+//         return true;
+//     }
+//     else{
+//         alert("Please input numeric characters only");
+//         return false;
+//     }
+// }
+
+
+// function userid_validation(){
+//     var uid = form1.userid;
+//     var uid_len = uid.value.length;
+
+//     if(uid_len == 0 || uid_len < 6 || uid_len > 10){
+//         alert("It should not be empty / length be between 6 to 10");
+//         return false;
+//     }
+//     alert("successfully entered user id");
+//     return true;
+// }
+
+
+
+// function phonenumber(inputtxt){
+//     var phoneno = /^\d{10}$/;
+//     if(inputtxt.value.match(phoneno)){
+//         alert("successfuly entered phone number");
+//         return true;
+//     }
+//     else{
+//         alert("Not a vvalid Phone Number");
+//         return false;
+//     }
+// }
+
 
 

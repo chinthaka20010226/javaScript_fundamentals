7eeef87161a3f1c8b0374cdf3d8d59c738ed7fca
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-03-30 14:41:06 +0530

    form validation with css

--- js.js
+++ js.js
@@ -121,3 +121,97 @@
 
 
 
+// validation form
+
+
+function com_password(inputText01,inputText02){
+    if(inputText01 == inputText02){
+        return true;
+    }
+    alert("your password is wrrong");
+    return false;
+}
+
+function check_email(inputText){
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    var email = inputText;
+    if(reg.test(email) == false){
+        return false;
+    }
+    return true;
+}
+
+function check_sex(sex){
+    x = 0;
+    if(sex[0].checked){
+        x++;
+    }
+    if(sex[1].checked){
+        x++;
+    }
+    if(x == 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function check_country(inputText){
+    if(inputText == "Default"){
+        return false;
+    }
+    return true;
+}
+
+function form_validation(form_01){
+    var uName = form_01.username.value;
+    var uPassword = form_01.password.value;
+    var uConPassword = form_01.con_password.value;
+    var uAddress = form_01.address.value;
+    var uEmail = form_01.email.value;
+    var umSex = form_01.sex;
+    var ufeSex = form_01.sex;
+    var uCountry = form_01.country.value;
+    var a = true;
+    var b = true;
+    var c = true;
+    var d = true;
+    var e = true;
+    var returnValue = false;
+
+    if(uName = "" || uPassword == "" || uConPassword == "" || uAddress == "" || uEmail == ""){
+        alert("there are empty fields, please fill them");
+        a = false;
+    }
+
+    var cPassword = com_password(uPassword,uConPassword);
+    if(cPassword == false){
+        b = false;
+    }
+
+    var email = check_email(uEmail);
+    if(email == false){
+        c = false;
+    }
+
+    var sex = check_sex(umSex);
+    if(sex == false){
+        alert("Gender should not empty");
+        d = false;
+    }
+
+    var country = check_country(uCountry);
+    if(country == false){
+        alert("Country is not selected")
+        e = false;
+    }
+
+    if(a == true && b == true && c == true && d == true && e == true){
+        alert("your form is successfuly");
+        returnValue = true;
+    }
+
+    return returnValue;
+
+}
